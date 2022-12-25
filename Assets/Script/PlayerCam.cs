83bb7c6b2cf7d988541fb14caf2d1447e1b41da8
Antony David <antonydavid945@gmail.com>
2022-12-25 20:23:36 +0100

    Menu

--- Assets/Script/PlayerCam.cs
+++ Assets/Script/PlayerCam.cs
@@ -15,7 +15,7 @@ public class PlayerCam : MonoBehaviour
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        Cursor.visible = true;
     }
 
     private void Update()
