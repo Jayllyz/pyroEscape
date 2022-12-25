aa8e7d8157ee539896139037929e80e9fe98e274
Antony David <antonydavid945@gmail.com>
2022-12-25 21:03:28 +0100

    event system

--- Assets/Script/PlayerCam.cs
+++ Assets/Script/PlayerCam.cs
@@ -12,12 +12,6 @@ public class PlayerCam : MonoBehaviour
     float xRotation;
     float yRotation;
 
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = true;
-    }
-
     private void Update()
     {
         float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
